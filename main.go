68364a4152e891a60035b03011918c46c03b4b66
talentedasian <lucenaalmirjames123@gmail.com>
2022-02-03 23:42:17 +0800

    Add degrees
    Add error message when unexpected type iss set

--- main.go
+++ main.go
@@ -9,10 +9,20 @@ import (
 )
 
 func main() {
+	tp := flag.String("type", "radians", "Specifies whether to use radians or degrees")
 	val := flag.Float64("value", 0, "The value to calculate")
 	flag.Parse()
 
-	calculateRad(*val)
+	switch *tp {
+	case "radians":
+		fmt.Println("\033[1m\033[33mTHIS IS CALCULATING IN \"RADIANS\"\033[0m")
+		calculateRad(*val)
+	case "degrees":
+		fmt.Println("\033[1m\033[33mTHIS IS CALCULATING IN \"DEGREES\"\033[0m")
+		calculateDeg(*val)
+	default:
+		fmt.Println("\033[31mPlease specify a viable type\033[0m")
+	}
 }
 
 func calculateRad(val float64) {
@@ -31,4 +41,10 @@ func calculateRad(val float64) {
 
 	fmt.Printf("Sine: %f\nCosine: %f\nTangent: %f\nCotangent: %f\nSecant: %f\nCosecant: %f\n\033[0m",
 		sin, cos, tan, cot, sec, csc)
+}
+
+func calculateDeg(val float64) {
+	pi := 3.14159265359
+	deg := val * (pi / 180)
+	calculateRad(deg)
 }
